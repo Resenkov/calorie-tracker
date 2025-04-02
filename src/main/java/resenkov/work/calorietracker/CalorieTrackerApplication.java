121f81aa8bf4a26ad455284f8d29ced221b8bfa8
Resenkov <roma.esenkov@yandex.ru>
2025-04-02 16:55:48 +0300

    init commit

---
+++ src/main/java/resenkov/work/calorietracker/CalorieTrackerApplication.java
@@ -0,0 +1,13 @@
+package resenkov.work.calorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/resenkov/work/calorietracker/CalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package resenkov.work.calorietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
